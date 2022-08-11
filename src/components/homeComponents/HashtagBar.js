91d94726b329370941d86c3a8a792a36f366067a
ninaschramm <schrammnina@outlook.com>
2022-08-11 17:37:19 -0300

    minor bug fix

--- src/components/homeComponents/HashtagBar.js
+++ src/components/homeComponents/HashtagBar.js
@@ -12,7 +12,7 @@ export default function HashtagBar() {
     const [hashtags, setHashtags] = useState(null);
 
     useEffect(() => {
-        const URL = "localhost:5000/hashtags";
+        const URL = "http://localhost:5000/hashtags";
         const promise = axios.get(URL);
         promise.then((res)=> {
             setHashtags(res.data);
