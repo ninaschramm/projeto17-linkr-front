6c51e39ef2ff7e25078b365c06dd6c25e641c847
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-08-19 19:39:28 -0300

    atualise posts incompleto

--- src/components/homeComponents/TimeLine.js
+++ src/components/homeComponents/TimeLine.js
@@ -7,13 +7,16 @@ import HashtagBar from './HashtagBar';
 import CreatePostCard from './CreatePostCard';
 import UserContext from '../../contexts/UserContext';
 import Modal from './Modal';
+import useInterval from 'use-interval'
 
 export default function TimeLine() {
     const { isModalVisible, deleteId } = useContext(UserContext)
     const UserInfo = JSON.parse(localStorage.getItem('UserInfo'));
 
     const [posts, setPosts] = useState(null);
-
+    const [newPosts, setNewPosts] = useState(null);
+    const [qtdNewPosts, setQtdNewPosts] = useState(0);
+    
     useEffect(() => {
         const URL = `${process.env.REACT_APP_API_BASE_URL}/posts`;
         const config = 
@@ -44,6 +47,41 @@ export default function TimeLine() {
         }
     }
 
+    function checkNewPosts(){
+        const URL = `${process.env.REACT_APP_API_BASE_URL}/posts`;
+        const config = 
+        {
+            headers:{
+            'Authorization': `Bearer ${UserInfo.token}`,
+            }
+        }
+        const promise = axios.get(URL, config);
+        promise.then((res)=> {
+            setNewPosts(res.data);
+        })
+        promise.catch((err) => {setPosts('error'); console.log(err.response.data)});
+        compareNewPost();
+    }
+
+    function compareNewPost(){
+        if(posts && newPosts){
+            if(newPosts.length > posts.length){
+                return setQtdNewPosts(newPosts.length - posts.length);
+            }
+        }
+    }
+    
+    function atualisePosts(){
+        console.log('atualizando');
+        let start = newPosts.length - posts;
+        let arrcerto = [];
+        for(let i = 0; i <(newPosts.length - posts); i++){
+            arrcerto.push(newPosts[start+i]);
+        }
+        setPosts([arrcerto,posts]);
+    }
+
+    useInterval(checkNewPosts, 15000);
 
     const callShowPosts = showPosts()
 
@@ -53,6 +91,11 @@ export default function TimeLine() {
 			<Container>
                 <Title>timeline</Title>
                 <CreatePostCard />
+                {(qtdNewPosts > 0)?
+                    <AtualisePosts onClick={atualisePosts}>
+                        {qtdNewPosts} posts pra carregar
+                    </AtualisePosts>
+                :``}
                 {callShowPosts}
 			</Container>
             <Sidebar>
@@ -91,4 +134,7 @@ const Title = styled.div`
 
 const Sidebar = styled.div`
     margin-top: 171px;
+`
+const AtualisePosts = styled.button`
+
 `
